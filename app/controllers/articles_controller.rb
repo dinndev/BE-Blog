6db9528a73b1c0165993d2efd1a33fb6c4979314
din <dinndev@gmail.com>
2022-04-23 07:15:42 +0800

    :rocket: Create article feature added
    Create article feature and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,31 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:d])
+    end
+
+    def new
+        @article = Article.new
+    end
+    
+    def create
+       @article = Article.new
+       @article.title = params[:title]
+       @article.body = params[:body]
+
+       if @article.save
+         redirect_to articles_path
+       else
+         render :new
+       end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'comments/index'
+  root 'articles#index'
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create', as: 'create_article'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+ 
