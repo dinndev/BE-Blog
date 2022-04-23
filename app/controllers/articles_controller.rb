80c05e8b8b6ca3ff943026d4df2f8d67db6b91df
din <dinndev@gmail.com>
2022-04-23 08:58:46 +0800

    :rocket: Destroy article anf fix
    destroy article and fix the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,28 +4,44 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:d])
+        @article = Article.find(params[:id])
     end
 
     def new
         @article = Article.new
     end
     
-    def create
-       @article = Article.new
-       @article.title = params[:title]
-       @article.body = params[:body]
+    def edit
+        @article = Article.find(params[:id])
+      end
+    
+      def update
+        @article = Article.find(params[:id])
+    
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
 
+    def create
+        @article = Article.new(article_params)
        if @article.save
          redirect_to articles_path
        else
-         render :new
+        render :new
        end
     end
 
     def destroy
-      @article = Article.find(params[:id])
-      @article.destroy
-      redirect_to articles_path
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path, status: :see_other
+    end 
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  get '/articles' => 'articles#index'
-  get '/articles/new' => 'articles#new', as: 'new_article'
-  post '/articles' => 'articles#create', as: 'create_article'
+  # root 'articles#index'
+  # get '/articles' => 'articles#index'
+  # get '/articles/new' => 'articles#new', as: 'new_article'
+  # post '/articles' => 'articles#create', as: 'create_article'
+  # get '/articles/:id' => "articles#show" 
+resources :articles 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
